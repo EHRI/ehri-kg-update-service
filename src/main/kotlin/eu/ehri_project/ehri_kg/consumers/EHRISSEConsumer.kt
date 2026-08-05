f7b24481af20a55fe3708b2763addf68b14889d3
Herminio García González <herminiogg@hotmail.com>
2026-08-05 09:17:21 +0200

    Added the option to resume from a certain event and improved the reactive processing. Closes #9.

--- src/main/kotlin/eu/ehri_project/ehri_kg/consumers/EHRISSEConsumer.kt
+++ src/main/kotlin/eu/ehri_project/ehri_kg/consumers/EHRISSEConsumer.kt
@@ -3,17 +3,26 @@ package eu.ehri_project.ehri_kg.consumers
 import com.herminiogarcia.shexml.streaming.StreamMappingLauncher
 import com.herminiogarcia.shexml.streaming.helpers.ReactiveConverters
 import eu.ehri_project.ehri_kg.helpers.SourceHelper
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.core.Observable
 import org.apache.jena.query.Dataset
 
 
-class EHRISSEConsumer(mappingRulesPath: String) {
+class EHRISSEConsumer(mappingRulesPath: String, val lastEventId: String? = null) {
     val mappingRules: String = SourceHelper.readFile(mappingRulesPath)
 
-    fun processEvents(): Single<Observable<Dataset>> {
+    fun processEvents(): Single<Flowable<Dataset>> {
+        val finalMappingRules = lastEventId?.let {
+            mappingRules.lines().joinToString("\n") {
+                if (it.startsWith("STREAM"))
+                    it.replaceFirst(">", "?Last-Event-Id=$lastEventId>")
+                else it
+            }
+        } ?: mappingRules
         return ReactiveConverters.convertToRxJava(
-            StreamMappingLauncher(false, true).launchMapping(mappingRules))
+            StreamMappingLauncher(false, true).launchMapping(finalMappingRules)
+        ).map { it.toFlowable(BackpressureStrategy.BUFFER) }
     }
 
 }
